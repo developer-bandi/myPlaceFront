42d57de9a1dee2612295aa75e656f572d8a95144
김상두 <puki4416@gmail.com>
2022-11-27 00:28:32 +0900

    chore : jest 설정파일 덮어씌워진 설정 변경

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    ".+\\.(scss)$": "identity-obj-proxy",
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/mocks/fileMock.ts",
   },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
