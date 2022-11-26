f819de0bdbf9737081068543ff219e4c9510f56f
김상두 <puki4416@gmail.com>
2022-11-26 14:53:41 +0900

    chore : storybook 의존성 추가 및 세팅

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  stories: [
+    "../component/**/*.stories.mdx",
+    "../component/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "storybook-addon-next",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+};
