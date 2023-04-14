6d339c5eb4ada2066fb5b0351927eb480c47837b
김상두 <puki4416@gmail.com>
2023-04-14 14:24:02 +0900

    fix : JSON 변환을 배열과 문자열이 아닌경우 수행하도록 수정

--- api/lib/makeForm.ts
+++ api/lib/makeForm.ts
@@ -1,18 +1,20 @@
 const makeForm = (AppendInfo: { [key: string]: unknown }) => {
   const formData = new FormData();
-
   Object.entries(AppendInfo).forEach(([key, value]) => {
     if (Array.isArray(value)) {
       for (let i = 0; i < value.length; i++) {
-        formData.append(`${key}[]`, JSON.stringify(value[i]));
+        formData.append(`${key}[]`, value[i]);
       }
     } else {
       if (value !== undefined) {
-        formData.append(key, JSON.stringify(value));
+        if (typeof value === "string") {
+          formData.append(key, value);
+        } else {
+          formData.append(key, JSON.stringify(value));
+        }
       }
     }
   });
-
   return formData;
 };
 
